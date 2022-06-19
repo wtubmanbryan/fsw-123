574e8ff9e2390719054ff164e90bf3e5d87f7622
wtubmanbryan <william.tubman@bryanuniversity.edu>
2022-06-19 00:49:41 -0400

    Added Todo Part2 Adding Props

---
+++ wk1/todolist/src/components/Todo.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Todo(props) {
+  return (
+    <div>{props.text}</div>
+  )
+}
+
+export default Todo
---
+++ wk3/STORE.js
@@ -0,0 +1,8 @@
+  
+       export const todos = [
+            {id: 1, text: "Goto Gym"},
+            {id: 2, text: "Do laundary"},
+            {id: 3, text: "Food Shopping"},
+            {id: 4, text: "Do Assignment"},
+            {id: 5, text: "Watch movie"}
+        ];
